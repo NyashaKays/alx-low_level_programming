97ad0cfe49301da86b0ee22dae641b66284baae0
NyashaKays <nyashadzashekatsidzira@gmail.com>
2023-02-16 22:34:44 +0200

    standard tasks

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	printf("Sixe of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of long long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu bytes(s)\n",haakana
+	return (o);
+}
