b57eac127b0eead0d21f691c28458f1bfcd8980e
NyashaKays <nyashadzashekatsidzira@gmail.com>
2023-02-17 15:23:31 +0200

    mandatory tasks

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,10 +4,10 @@
 
 /**
  * main - Prints a random number and states whether
- * 	it is postive, negative, or zero.
+ *        it is positive, negative, or zero.
  *
  * Return: Always 0.
- */
+*/
 int main(void)
 {
 	int n;
