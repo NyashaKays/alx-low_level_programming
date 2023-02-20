4f68c1e7f56adf68c19fdea4ef637e3f1fc66b8d
NyashaKays <nyashadzashekatsidzira@gmail.com>
2023-02-20 09:59:01 +0200

    corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main -Entry point
+ *  main -Entry point
  *
- * Return: Always 0 (Success)
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a mutilingual puzzle ");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
