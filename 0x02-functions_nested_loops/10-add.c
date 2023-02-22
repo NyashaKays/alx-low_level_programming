f4bfac0bc56007be978d175571e342fabae8813c
NyashaKays <nyashadzashekatsidzira@gmail.com>
2023-02-22 13:31:34 +0200

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
+
+    n = add(105, 25);
+    printf("%d\n", n);
+    return (0);
+}
