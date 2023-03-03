24f316997ae0d1da24956ee1ea0669995104cd39
NyashaKays <nyashadzashekatsidzira@gmail.com>
2023-03-04 01:07:58 +0200

    task 2 update

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,29 +1,26 @@
 #include "main.h"
 
 /**
- * _strncat - joins two strings with n bytes
- *  @dest: copy to
- *  @src: copy from
- *  @n: number of char to be copied
- *  Return: dest
+ *_strncpy - function that copies a string
+ *
+ * @src: The source of strings
+ * @dest: The destination of the string
+ * @n: The length of int
+ *
+ * Return: pointer to the resulting string dest
  */
-char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
-	int j;
+int i;
 
-	i = 0;
-	while (dest[i] != '\0')
+	for (i = 0; i < n && *(src + i); i++)
 	{
-		i++;
+		*(dest + i) = *(src + i);
 	}
-	j = 0;
-	while (j < n && src[j] != '\0')
+	for (; i < n; i++)
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+	*(dest + i) = '\0';
 	}
-	dest[i] = '\0';
 	return (dest);
+
 }
